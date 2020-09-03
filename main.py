6bad7edc10165c13b9c7dea603fec1479c3f2744
Chetanmitra <czm5805@psu.edu>
2020-09-03 23:57:08 +0000

    space

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#Author: Chetan Mitra czm5805@psu.edu
-#Collaborator: Tyler Ciuca tmc6093@psu.edu
-#Collaborator: Dymea Schippers dxs5940@psu.edu
-#Collaborator: Jian Hong Weng jpw6087@psu.edu
+# Author: Chetan Mitra czm5805@psu.edu
+# Collaborator: Tyler Ciuca tmc6093@psu.edu
+# Collaborator: Dymea Schippers dxs5940@psu.edu
+# Collaborator: Jian Hong Weng jpw6087@psu.edu
 
 
 temperature = float(input("Enter temperature: "))
